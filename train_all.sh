051a293063f733f1f498ae8b5188b22887164b5e
Gitea <gitea@fake.local>
2019-02-14 18:33:45 +0300

    up

--- train_all.sh
+++ train_all.sh
@@ -6,7 +6,7 @@ data_dir=data
 train_dir=train
 steps=500,500,500
 rate=0.0005,0.0001,0.00002
-unknown_percentage=50
+unknown_percentage=80
 dct_coefficient_count=10
 window_size_ms=40  
 window_stride_ms=20
@@ -28,7 +28,7 @@ function freeze {
     --window_size_ms=$window_size_ms               \
     --window_stride_ms=$window_stride_ms           \
     --checkpoint=$ckpt                             \
-    --output_file=$train_dir/$model/frozen.pb
+    --output_file=$train_dir/$1/frozen.pb
 }
 
 function train {
@@ -48,10 +48,11 @@ function train {
     --summaries_dir=$train_dir/$1/retrain_logs     \
     --train_dir=$train_dir/$1/training
     
-    freeze $1 $2
+    freeze $1 "$2"
 }
 
 #train ds_cnn "6 276 10 4 2 1 276 3 3 2 2 276 3 3 1 1 276 3 3 1 1 276 3 3 1 1 276 3 3 1 1"
 #train lstm "188 500"
 #train crnn "100 10 4 2 1 2 136 188"
-train gru  "1 400" 
+train gru "1 400"
+# freeze gru "1 400"
