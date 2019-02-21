ec0033cb44387b9d2c5b24f3cd4831a561f713e4
peko <gasu.basu@gmail.com>
2019-02-21 12:00:44 +0300

    commit

--- train_all.sh
+++ train_all.sh
@@ -10,7 +10,9 @@ unknown_percentage=80
 dct_coefficient_count=10
 window_size_ms=40  
 window_stride_ms=20
-clip_duration_ms=500
+clip_duration_ms=1000
+clip_stride_ms=30
+sample_rate=16000
 
 function freeze {
     
@@ -25,8 +27,10 @@ function freeze {
     --wanted_words=$words                          \
     --dct_coefficient_count=$dct_coefficient_count \
     --clip_duration_ms=$clip_duration_ms           \
+    --clip_stride_ms=$clip_stride_ms               \
     --window_size_ms=$window_size_ms               \
     --window_stride_ms=$window_stride_ms           \
+    --sample_rate=$sample_rate                     \
     --checkpoint=$ckpt                             \
     --output_file=$train_dir/$1/frozen.pb
 }
@@ -41,8 +45,11 @@ function train {
     --data_url=$data_url                           \
     --unknown_percentage=$unknown_percentage       \
     --dct_coefficient_count=$dct_coefficient_count \
+    --clip_duration_ms=$clip_duration_ms           \
+    --clip_stride_ms=$clip_stride_ms               \
     --window_size_ms=$window_size_ms               \
     --window_stride_ms=$window_stride_ms           \
+    --sample_rate=$sample_rate                     \
     --learning_rate=$rate                          \
     --how_many_training_steps=$steps               \
     --summaries_dir=$train_dir/$1/retrain_logs     \
@@ -51,9 +58,9 @@ function train {
     freeze $1 "$2"
 }
 
-# train ds_cnn "6 276 10 4 2 1 276 3 3 2 2 276 3 3 1 1 276 3 3 1 1 276 3 3 1 1 276 3 3 1 1"
-# train lstm "188 500"
-# train crnn "100 10 4 2 1 2 136 188"
-# train gru "1 400"
+train ds_cnn "6 276 10 4 2 1 276 3 3 2 2 276 3 3 1 1 276 3 3 1 1 276 3 3 1 1 276 3 3 1 1"
+train lstm "188 500"
+train crnn "100 10 4 2 1 2 136 188"
+train gru "1 400"
 train cnn "60 10 4 1 1 76 10 4 2 1 58 128"
 train dnn "436 436 436"
